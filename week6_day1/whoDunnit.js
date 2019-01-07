477a6acf7c5d5d8f740d89e411fa0b8305ba6f7a
Slawomir Dyk <slawomirdyk@gmail.com>
2019-01-07 22:48:43 +0000

    week_6-day_1 homework

---
+++ week6_day1/whoDunnit.js
@@ -0,0 +1,66 @@
+Episode 1
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+The console is going to print 'Miss Scarlet' as a murderer because each const is within the
+same scope
+
+
+Episode 2
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+Terminal will show an error, Line 17 uses const which means the variable 'murderer' can not be changed.
+
+Episode 3
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+It will return First Verdit: Mrs. Peacock and Second Verdict: Professor Plum. The function
+declareMurderer changes the value of the variable 'murderer' but this is only visible within the scope
+of the function declareMurderer. The secondVerdict  uses the variable 'murderer' on line 34 (?not sure)
+
+Episode 4
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
